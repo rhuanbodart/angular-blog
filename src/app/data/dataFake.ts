0cde2365400c7416161e5f66d006db50a5c33af9
Rhuan Bodart <126196894+rhuanbodart@users.noreply.github.com>
2023-08-31 14:16:26 -0300

    content-components

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+    {
+        "id":"1",
+        "title": "O mundo da programação",
+        "description": "Este mundo é realmente incrível. A medida que você vai crescendo em conhecimento, vai adquirindo novas habilidades capazes de te levar a um nível ainda mais profundo na tecnologia!",
+        "photoCover": "https://www.mooc.org/hubfs/applications-of-computer-programming.jpg"
+    },
+    {
+        "id":"2",
+        "title": "Desenvolvendo aplicação de blog utilizando o Framework Angular",
+        "description":"",
+        "photoCover": "https://miro.medium.com/v2/resize:fit:1400/1*bMgQ8MhbnQexpqHgIgBJPA.png"
+    },
+    {
+        "id":"3",
+        "title": "O Typescript é um superset do javascript que auxilia na criação de componentes com funcionalidades especiais",
+        "description":"",
+        "photoCover": "https://static.wixstatic.com/media/c60f10_9605a49c09e948e6bf0195ac2738df23~mv2.png/v1/fill/w_560,h_316,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/c60f10_9605a49c09e948e6bf0195ac2738df23~mv2.png"
+    },
+    {
+        "id":"4",
+        "title": "O Banco Santander e a DIO arrasaram no curso de FullStack Java + Angular",
+        "description":"",
+        "photoCover": "https://media.licdn.com/dms/image/D4D22AQHZlmCk4B-Rww/feedshare-shrink_800/0/1692239006760?e=2147483647&v=beta&t=4NqLE1yQfAg0d4Q7vQyQATn5WKkJkzQXtlMri-PyDiQ"
+    }
+]
